188324af5aa56375e93cc764dda1098b35ea4643
olegs <olegsavinykh@yahoo.com>
2024-01-19 21:05:09 +0300

    KS-1-2 numberOfEmployees (-1)

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_1
+
+fun main () {
+
+    val aBottleOfWater  = 30
+    val wordsOfGratitude  = "Спасибо что выбрали наш магазин!"
+
+    println(wordsOfGratitude)
+    println(aBottleOfWater)
+
+    //val numberOfEmployees  = 2000
+    //println(numberOfEmployees)
+
+    val  numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+
+}
