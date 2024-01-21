3ccf94d7e2f8f8e25f0e809990cfdb2d3c0b86b7
olegs <olegsavinykh@yahoo.com>
2024-01-21 15:41:17 +0300

    KS-1-3 completing the third task

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -1,23 +1,12 @@
 package org.example.lesson_1
 
-fun main () {
+fun main() {
 
     val year = "1961"
     var hour = "9"
-    var minute = "7"
-
-
+    var minute = "07"
     println("$year   \n$hour  \n$minute")
-
-     hour  = "10"
-     minute  = "55"
-
-
-    print(hour + ":" + minute)
-
-
-
-
-
-
+    hour = "10"
+    minute = ":55"
+    print("$hour$minute")
 }
