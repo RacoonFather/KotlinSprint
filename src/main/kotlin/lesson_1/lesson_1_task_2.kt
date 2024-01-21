4aefe69fef8fbef35820e8af8ae40b436e5fc980
olegs <olegsavinykh@yahoo.com>
2024-01-21 15:26:21 +0300

    KS-1-2 completing the second task

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -1,17 +1,15 @@
 package org.example.lesson_1
 
-fun main () {
+fun main() {
 
-    val aBottleOfWater  = 30
-    val wordsOfGratitude  = "Спасибо что выбрали наш магазин!"
-
+    val aBottleOfWater = 30
+    val wordsOfGratitude = "Спасибо что выбрали наш магазин!"
     println(wordsOfGratitude)
     println(aBottleOfWater)
 
-    //val numberOfEmployees  = 2000
+    var numberOfEmployees = 2000
     //println(numberOfEmployees)
-
-    val  numberOfEmployees = 1999
+    numberOfEmployees = 1999
     println(numberOfEmployees)
 
 
